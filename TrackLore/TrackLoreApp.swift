bd1b7da846e690093e5caa6b33e7d3f8c2b6e3e5
Joabbastidas <joabbastiddas@gmail.com>
2025-06-25 18:55:02 -0400

    Initial Commit

---
+++ TrackLore/TrackLoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackLoreApp.swift
+//  TrackLore
+//
+//  Created by Joab Bastidas on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackLoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackLoreTests/TrackLoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackLoreTests.swift
+//  TrackLoreTests
+//
+//  Created by Joab Bastidas on 6/25/25.
+//
+
+import Testing
+@testable import TrackLore
+
+struct TrackLoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
